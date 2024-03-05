3380129071707858fc4f390483e72bf0e78141d8
Felipe Marques <felipe.comarques1208@gmail.com>
2024-03-05 10:49:03 -0300

    feat: integrate API in profile section
    Implemented API consumption within the profile section. Additionally, created environment variables to securely store the API key.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,58 @@
+import { useState, useEffect } from 'react';
 import { Badge } from "./ui/badge";
 
 const Profile = () => {
+    const [summonerData, setSummonerData] = useState(null);
+
+    useEffect(() => {
+        const fetchSummonerData = async () => {
+            try {
+                const proxy = "https://corsproxy.io/?"
+                const apiUrl = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
+                const playerName = "help%20me%20again"
+                const apiKey = process.env.REACT_APP_API_KEY
+
+                const response = await fetch(proxy + apiUrl + playerName, {
+                    headers: {
+                        'X-Riot-Token': apiKey
+                    }
+                });
+                if (!response.ok) {
+                    throw new Error('Falha ao buscar dados do jogador');
+                }
+                const data = await response.json();
+                setSummonerData(data);
+            } catch (error) {
+                console.error('Erro ao buscar dados do jogador:', error);
+            }
+        };
+
+        fetchSummonerData();
+    }, []);
+
+
     return (
         <div>
-            <div className="flex items-center gap-6">
-                <div className="flex-col">
-                    <img
-                        className="h-36 w-36 rounded-full border-2"
-                        src={`http://ddragon.leagueoflegends.com/cdn/11.5.1/img/profileicon/4500.png`}
-                        alt="Profile Picture"
-                    />
-                    <div className="flex justify-center mt-1">
-                        <Badge variant="secondary">000</Badge>
+            {summonerData && (
+                <div className="flex items-center gap-6">
+                    <div className="flex-col">
+                        <img
+                            className="h-36 w-36 rounded-full border-2"
+                            src={`http://ddragon.leagueoflegends.com/cdn/14.4.1/img/profileicon/${summonerData.profileIconId}.png`}
+                            alt="Profile Picture"
+                        />
+                        <div className="flex justify-center mt-1">
+                            <Badge variant="secondary">{summonerData.summonerLevel}</Badge>
+                        </div>
                     </div>
-                </div>
 
-                <h1 className="text-5xl text-[#F0E6D2] mb-6"> PlayerName
-                    <span className="text-xl text-[#F0E6D2] text-muted-foreground">#BR1</span>
-                </h1>
-            </div>
+                    <h1 className="text-5xl text-[#F0E6D2] mb-6"> {summonerData.name}
+                        <span className="text-xl text-[#F0E6D2] text-muted-foreground">#BR1</span>
+                    </h1>
+                </div>
+            )}
         </div>
-    )
+    );
 };
 
 export default Profile;
