125b88062e74d245794ce5b7af6b75297ef1fae9
Felipe Marques <felipe.comarques1208@gmail.com>
2024-03-06 14:23:59 -0300

    feat: add MatchDetails page
    Created the MatchDetails page to display details not shown by MatchCard.

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
         element: <HomePage />
       },
       {
-        path: "match",
+        path: "/match/:matchId/player/:playerId",
         element: <MatchDetails />
       }
     ]
